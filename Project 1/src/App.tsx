aa0246b31dba62a3f69366f659188683d98eb3e3
Thomas Sørensen <tsorensen007@gmail.com>
2025-09-07 11:00:55 +0200

    fix: remove empty card

--- Project 1/src/App.tsx
+++ Project 1/src/App.tsx
@@ -1,7 +1,6 @@
 import {useQuery} from '@tanstack/react-query';
 import {tmdbApi} from './services/tmdbApi';
 import MovieCard from './components/MovieCard';
-import type {Movie} from './types/movie';
 import './App.css';
 
 function App() {
@@ -30,36 +29,16 @@ function App() {
     );
   }
 
-  // Display only 1 movie as a demo plus a test movie with missing data
-  const movies = data?.results.slice(0, 1) || [];
-
-  // Add minimal test movie with missing data
-  const testMovie: Movie = {
-    id: 999999,
-    title: '',
-    poster_path: null,
-    release_date: '',
-    vote_average: 0,
-    overview: '',
-    backdrop_path: null,
-    vote_count: 0,
-    genre_ids: [],
-    popularity: 0,
-    adult: false,
-    original_language: '',
-    original_title: '',
-  };
-
-  const allMovies = [...movies, testMovie];
+  const movies = data?.results || [];
 
   return (
     <main className="app">
       <header className="app-header">
         <h1>Movie Card Demo</h1>
-        <p>Displaying 1 popular movie + 1 test movie with missing data</p>
+        <p>Displaying popular movies from TMDB</p>
       </header>
       <section className="app-main">
-        {allMovies.map((movie) => (
+        {movies.map((movie) => (
           <MovieCard key={movie.id} movie={movie} />
         ))}
       </section>
